d422276dbd3e0e2fabcf6f7a9df46131255640e0
TheLittleBoy <wangxa@edu-edu.com.cn>
2015-11-18 17:32:35 +0800

    first commit

--- IP/ViewController.swift
+++ IP/ViewController.swift
@@ -7,12 +7,48 @@
 //
 
 import UIKit
+import Alamofire
 
 class ViewController: UIViewController {
 
+    
+    let myIpUrl = "http://ip.taobao.com/service/getIpInfo2.php?ip=myip"
+    var request: Alamofire.Request?
+    
+    @IBOutlet weak var myIpLabel: UILabel!
+    
+    @IBOutlet weak var myActivityIndicatorView: UIActivityIndicatorView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+
+        self.requestMyIpInfo()
+        
+    }
+    
+    func requestMyIpInfo() {
+        
+        request?.cancel()
+        
+        self.myIpLabel.text = "N/A"
+        self.myActivityIndicatorView.startAnimating()
+        
+        request = Alamofire.request(.GET, myIpUrl).responseJSON{ response in
+            
+            if let JSON = response.result.value {
+                print("JSON: \(JSON)")
+                
+                if let data = JSON["data"] as? NSDictionary
+                {
+                    let ip = data["ip"]
+                    print("my ip is : \(ip)")
+                    
+                    self.myIpLabel.text = ip as? String
+                }
+                
+                self.myActivityIndicatorView.stopAnimating()
+            }
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +56,11 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func refreshButtonPressed(sender: UIButton) {
+        
+        self.requestMyIpInfo()
+        
+    }
 
 }
 
