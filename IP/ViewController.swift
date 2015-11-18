67a24b75b9b9038ab384c858a99ade9892502d2a
TheLittleBoy <wangxa@edu-edu.com.cn>
2015-11-18 15:34:59 +0800

    Initial Commit

---
+++ IP/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  IP
+//
+//  Created by  MAC on 15/11/18.
+//  Copyright © 2015年 华夏大地教育. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
